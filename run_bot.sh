57ab8eeec87ded504827236893b89c3b622add9e
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-10-27 11:17:04 -0500

    Added an argument that specifies a Firebase UID

--- run_bot.sh
+++ run_bot.sh
@@ -2,5 +2,6 @@
 # Run the CJSKA Sync Bot
 
 FIREBASE_SECRET=MY_FIREBASE_SECRET # TODO: Replace "MY_FIREBASE_SECRET" with Firebase Secret before running
+FIREBASE_USERID=MY_FIREBASE_UID # TODO: Replace "MY_FIREBASE_UID" with the correct Firebase UID before running
 
-python3 src/bot.py $FIREBASE_SECRET
+python3 src/bot.py $FIREBASE_SECRET $FIREBASE_USERID
