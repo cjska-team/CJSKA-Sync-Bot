d00c86b3df3ffdf199e43977e8c4283723458bc7
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-10-27 11:17:46 -0500

    Preparing to replicate old bot

--- src/bot.py
+++ src/bot.py
@@ -1,16 +1,20 @@
 import sys
 import requests
 import json
+from firebase_token_generator import create_token
 
-def main(firebaseSecret):
-    if firebaseSecret != "MY_FIREBASE_SECRET":
-        print("TODO (Gigabyte Giant): Write this!")
+def getFirebaseToken(secret, uid):
+    return create_token(secret, {"uid": uid})
+
+def main(firebaseSecret, firebaseUID):
+    if firebaseSecret != "MY_FIREBASE_SECRET" and firebaseUID != "MY_FIREBASE_UID":
+        fbToken = getFirebaseToken(firebaseSecret, firebaseUID)
     else:
-        print("Uh-oh! It looks like you forgot to include a valid Firebase Secret.")
+        print("Uh-oh! It looks like you forgot to include a valid Firebase Secret and/or a valid Firebase UID.")
         return
 
 print("Command Line arguments: " + str(sys.argv))
 try:
-    main(str(sys.argv[1]))
+    main(str(sys.argv[1]), str(sys.argv[2]))
 except IndexError:
     print("Sorry, we couldn't find the correct command line argument. Exiting!")
